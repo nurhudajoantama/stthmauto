3d348ede5b7c2ecc576679ce6c51ab5ba488dbb5
nurhudajoantama <nurhuda.joantama14@gmail.com>
2025-11-17 21:38:06 +0700

    tambah post

--- app/hmalert/handler.go
+++ app/hmalert/handler.go
@@ -1,6 +1,7 @@
 package hmalert
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/nurhudajoantama/hmauto/app/server"
@@ -18,8 +19,14 @@ func RegisterHandler(s *server.Server, svc *HmalerService) {
 
 	r := s.GetRouter()
 	hmalertGroup := r.PathPrefix("/hmalert").Subrouter()
-	hmalertGroup.HandleFunc("/publish", h.PublishAlert).Methods("GET")
+	hmalertGroup.HandleFunc("/publish", h.PublishAlert).Methods("GET", "POST")
 
+}
+
+type publishAlertRequest struct {
+	Level   string `json:"level"`
+	Message string `json:"message"`
+	Tipe    string `json:"tipe"`
 }
 
 func (h *HmalertHandler) PublishAlert(w http.ResponseWriter, r *http.Request) {
@@ -27,10 +34,35 @@ func (h *HmalertHandler) PublishAlert(w http.ResponseWriter, r *http.Request) {
 	l := zerolog.Ctx(ctx)
 	l.Info().Msg("Handling PublishAlert request")
 
-	q := r.URL.Query()
-	level := q.Get("level")
-	message := q.Get("message")
-	tipe := q.Get("tipe")
+	var level, message, tipe string
+
+	switch r.Method {
+	case http.MethodGet:
+		q := r.URL.Query()
+		level = q.Get("level")
+		message = q.Get("message")
+		tipe = q.Get("tipe")
+	case http.MethodPost:
+		// parse from json
+		body := r.Body
+		defer body.Close()
+		decoder := json.NewDecoder(body)
+		err := decoder.Decode(&publishAlertRequest{
+			Level:   level,
+			Message: message,
+			Tipe:    tipe,
+		})
+		if err != nil {
+			l.Error().Err(err).Msg("Failed to parse request body")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	default:
+		l.Error().Msg("Unsupported HTTP method")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	l.Debug().Str("level", level).Str("message", message).Str("type", tipe).Msg("Parsed PublishAlert request")
 
 	err := h.Service.PublishAlert(ctx, tipe, level, message)
 	if err != nil {
